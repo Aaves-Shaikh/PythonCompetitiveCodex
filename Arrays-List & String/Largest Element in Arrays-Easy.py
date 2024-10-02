1cf016b483618c4a3dfea8d43b7f08bcc12c04be
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-02 18:55:09 +0530

    Create Largest Element in Arrays-Easy.py
    1st- Problem of easy level to find the Largest Element

---
+++ Arrays-List & String/Largest Element in Arrays-Easy.py
@@ -0,0 +1,23 @@
+# Largest Element in Array 
+# Link to the problem https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+# Given an array, arr. The task is to find the largest element in it.
+# Date: 02/10/2024
+# The Problem Consist of two solution. A better Solution and A optimal Solution. 
+# the better Soln
+def missingNumber(n,arr): #better soln 
+    num_set=set(arr)
+    for i in range(1, n+1):
+        if i not in num_set:
+            print(i)
+n=int(input())
+arr=list(map(int,input().split()))
+missingNumber(n,arr)
+
+# optimal soln 
+def missingNumberOpt(n1,arr1): 
+    num_set=sum(arr1)
+    s2=n1*(n1+1)//2
+    print(abs(num_set-s1))
+n1=int(input())
+arr1=list(map(int,input().split()))
+missingNumberOpt(n1,arr1)
