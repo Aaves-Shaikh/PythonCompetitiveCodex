cd384008e2368126803b921aff47c00f7930eb0b
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-07 20:26:54 +0530

    Created medium-gfg-Kadane's Algorithm.py

--- Arrays-List & String/medium-gfg-Kadane's Algorithm.py
+++ Arrays-List & String/medium-gfg-Kadane's Algorithm.py
@@ -3,7 +3,6 @@
 # Given an array, arr. The task is to find the sub array of the element in it.
 # Date: 07/10/2024
 # The Problem Consist of three solution. Brute force solution, better Solution, and A optimal Solution. 
-
 # Method 1 the  Brute Force Method 
 import sys
 def maxsubarray_sum(arr):
@@ -34,3 +33,18 @@ def max_subaaraysum(A):
 A=[-2,-1.-3,4,-1,2,1,-5,-4] 
 maxsubarray_sum(A)
 
+# Method 3: here we use the Kadaen's algorithm
+def maxSubarraySum(array):
+    maxi = sys.maxsize -1
+    sum=0
+    for i in range(len(arrary)):
+        sum+=array[i]
+        if sum>maxi:
+            maxi=sum
+        if sum <0:
+            sum=0
+        if maxi<0:
+            maxi=0
+    print(maxi)
+array=[-2,-1.-3,4,-1,2,1,-5,-4] 
+maxSubarraySum(array):
