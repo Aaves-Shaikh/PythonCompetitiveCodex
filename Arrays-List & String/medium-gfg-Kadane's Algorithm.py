e39920e9a583dc68f707e85148a33dd4936a59eb
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-07 20:23:27 +0530

    Update medium-gfg-Kadane's Algorithm.py

--- Arrays-List & String/medium-gfg-Kadane's Algorithm.py
+++ Arrays-List & String/medium-gfg-Kadane's Algorithm.py
@@ -4,7 +4,7 @@
 # Date: 07/10/2024
 # The Problem Consist of three solution. Brute force solution, better Solution, and A optimal Solution. 
 
-# Method 1 the better Soln using Brute Force Method 
+# Method 1 the  Brute Force Method 
 import sys
 def maxsubarray_sum(arr):
     maxi= -sys.maxsize -1
@@ -19,3 +19,18 @@ def maxsubarray_sum(arr):
     print(maxi)
 arr=[-2,-1,-3,-1,-5,-4] 
 maxsubarray_sum(arr)  
+
+#Method2 the better Soln here we optimize the code by removing the 3 for loop
+def max_subaaraysum(A):
+    maxi= -sys.maxsize -1
+    for i in range( len(arr)):
+        sum=0
+        for j in range(i,len(arr)):
+            sum+=arr[j]
+        maxi=max(maxi,sum)
+        if maxi<0:
+            maxi=0
+    print(maxi)
+A=[-2,-1.-3,4,-1,2,1,-5,-4] 
+maxsubarray_sum(A)
+
