c3a44669418ca2c9a4d0f08346c00bed621af041
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-08 20:20:59 +0530

    Update with Optimal Solution

--- Arrays-List & String/medium-leetCode-Search-2D-Matrix.py
+++ Arrays-List & String/medium-leetCode-Search-2D-Matrix.py
@@ -41,3 +41,24 @@ mat=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 tar= 1
 result=searchMatrix(mat,tar)
 print("Element found!!! " if result else "Element not found")
+
+# ==================== Better Solution ====================
+def search_matrix(M,T):
+    p=len(M)
+    q=len(M[0])
+    low,high=0,p*q-1
+    while low<=high:
+        mid=(low+high)//2
+        row=mid//q
+        col=mid%q
+        if M[row][col]==T:
+            return True
+        elif M[row][col]<T:
+            low =mid+1
+        else:
+            high=mid-1
+    return False
+M=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+T=16
+result=search_matrix(M,T)
+print("Element found!!! " if result else "Element not found")
