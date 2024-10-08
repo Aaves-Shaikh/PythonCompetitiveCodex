7f1efdfba4a233327f8e1b0cc2c90b9b1d21fb53
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-08 20:19:50 +0530

    Update and rename medium-LeetCode-Search-2D-Matrix.py to medium-leetCode-Search-2D-Matrix.py

--- Arrays-List & String/medium-leetCode-Search-2D-Matrix.py
+++ Arrays-List & String/medium-leetCode-Search-2D-Matrix.py
@@ -17,3 +17,27 @@ def searchmatrix(matrix,target):
 matrix= [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 target= 60
 searchmatrix(matrix,target)
+# ==================== Better Solution ====================
+def binarySearch(nums,tar):
+    n=len(nums)
+    low,high=0,n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==tar:
+            return True
+        elif nums[mid]<tar:
+            low=mid+1
+        else:
+            high=mid-1
+    return False
+def searchMatrix(mat,tar):
+    n=len(mat)
+    m=len(mat[0])
+    for i in range(len(mat)):
+        if mat[i][0]<= tar <= mat[i][m-1]:
+            return binarySearch(mat[i],tar)
+    return False
+mat=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+tar= 1
+result=searchMatrix(mat,tar)
+print("Element found!!! " if result else "Element not found")
