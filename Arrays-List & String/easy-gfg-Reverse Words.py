f5154d5c971a5a61202083ca776188a3024ae1d8
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-10 23:28:16 +0530

    Created easy-gfg-Reverse Words.py

--- Arrays-List & String/easy-gfg-Reverse Words.py
+++ Arrays-List & String/easy-gfg-Reverse Words.py
@@ -3,4 +3,11 @@
 # Given an space seprated string. cound the words in a comma seprated way
 # Date: 09/10/2024
 # The Problem Consist on one solution as of now . 
-# ==================== Better Solution ====================
+# ==================== Otimal Solution ====================
+def reverse_words(str):
+  words=str.split(".")
+  words=[::-1]
+  reverse_str=".".join(words)
+  print(reverse_str)
+str= "Solving a problem every day increases your coding skills"
+reverse_words(str)
