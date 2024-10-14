93a592b8fe9ce6a754e80b82caf9c08788506c0a
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-14 19:16:17 +0530

    Create easy-gfg-Duplicate Elements.py

---
+++ Arrays-List & String/easy-gfg-Duplicate Elements.py
@@ -0,0 +1,20 @@
+# Duplicate Elements
+# Given an array. The task is to find the duplicate elements and print them.
+# Date: 14/10/2024
+# The Problem Consist of three solution. A brute force Solution, a better solution, and a optimal Solution. 
+# Metod1: using the for loop we check if the element is present in the list is yes to print it
+from collections import Counter
+def sortedDuplicate(n,arr):
+  d=Counter(arr)
+  new=[]
+  for i in range(n):
+    if d[i]>1:
+      i.append(new)
+     if new:
+       new.sort()
+       for j in new:
+         print(j, end=" ")
+    else:
+      print(-1)
+n=9
+arr= [3, 4, 5, 7, 8, 1, 2, 1, 3]
