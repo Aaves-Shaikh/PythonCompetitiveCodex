056b4e81c22559b9f96667306101f3f9e92bd28f
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-21 22:00:33 +0530

    Create medium-gfg-alternate-pos-neg.py

---
+++ Arrays-List & String/medium-gfg-alternate-pos-neg.py
@@ -0,0 +1,34 @@
+# Alternate positive and negative numbers
+# Link to the problem https://www.geeksforgeeks.org/problems/array-of-alternate-ve-and-ve-nos1401/1
+# Given an array. The task is to place positive and negative numbers alternate
+# Date: 21/10/2024
+# The Problem Consist of Brute force solution
+# Method 1 the  Brute Force Method 
+# if numbers of positive elements are not equal to number of negative elements 
+def rearragnge(arr):
+    pos=[]
+    neg=[]
+    for i in range(len(arr)):
+        if arr[i]>=0:
+            pos.append(arr[i])
+        else:
+            neg.append(arr[i])
+    if len(pos)<len(neg):
+        for i in range(len(pos)):
+            arr[i*2]=pos[i]
+            arr[i*2+1]=neg[i]
+        index=len(pos)*2
+        for i in range(len(neg)-len(pos)):
+            arr[index]=neg[len(pos)+i]
+            index +=1
+    else:
+        for i in range(len(neg)):
+            arr[i*2]=pos[i]
+            arr[i*2+1]=neg[i]
+        index=len(neg)*2
+        for i in range(len(pos)-len(neg)):
+            arr[index]=pos[len(neg)+i]
+            index +=1
+    print(arr)
+arr=[9, 4, -2, -1, 5, 0, -5, -3, 2]
+rearragnge(arr)
