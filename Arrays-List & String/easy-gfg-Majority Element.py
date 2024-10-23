5a7783ea96e3e1213f493f7ecb2d6604dfbb739c
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-10-23 20:04:09 +0530

    Update easy-gfg-Majority Element.py

--- Arrays-List & String/easy-gfg-Majority Element.py
+++ Arrays-List & String/easy-gfg-Majority Element.py
@@ -3,7 +3,8 @@
 # Date: 23/10/2024
 # the probelm link https://www.geeksforgeeks.org/problems/majority-element-1587115620/1 
 # The Problem Consist of three solution. A brute force Solution, a better solution and a optimal Solution. 
-# Metod1: using the for loop we check if the element is present in the list if no we append it else we move forward
+# Metod1:
+from collections import Counter
 def majorityElement(arr):
   n=len(arr)//2
   for i in range(len(arr)):
@@ -17,3 +18,16 @@ def majorityElement(arr):
         print("No majority elements!!!")
 arr=[1,1, 8, 7, 9,56, 90,99,99,99]
 majorityElement(arr)
+
+# method 2: using counter
+def majorityelem(A):
+  N=len(A)//2
+  counter=counter(A)
+  for num, c in counter.items():
+    if c>N:
+      print("the majority element is:",num,"and it's count is:",c)
+            return 
+  print("No majority elements!!!")
+  return
+A=[1,1, 8,7,9,56, 90,99,99,99,99,99,99,99,99,99]
+majorityelem(A)
