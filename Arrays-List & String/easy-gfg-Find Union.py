fce1f77185094d72afd77ac23d140deb0ad6b4f6
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-11-02 19:19:38 +0530

    Create easy-gfg-Find Union.py

---
+++ Arrays-List & String/easy-gfg-Find Union.py
@@ -0,0 +1,15 @@
+# Union in the array
+# Given an two array A and B. The task is to find the union of those elements.
+# Date: 02/11/2024
+# The Problem Consist of Two solution. A brute force Solution, and a optimal Solution. 
+# Metod1: 
+ def union(A,B):
+    union=[]
+    for i in A+B:
+        if i not in union:
+            union.append(i)
+            union.sort()
+    print(f"The union elements in array A and B are {union}")
+A=[4, 5, 6, 5, 7, 5, 2, 1, 65, 3, 22 ]
+B=[1,2,3,4,5,6,7,8,9]
+union(A,B)
