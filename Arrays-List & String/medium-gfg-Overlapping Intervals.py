0809f742cf4a29dee9f895b63bbdff1672c9e074
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-11-11 19:03:52 +0530

    Create medium-gfg-Overlapping Intervals.py

---
+++ Arrays-List & String/medium-gfg-Overlapping Intervals.py
@@ -0,0 +1,23 @@
+# Overlapping-intervals
+# Link to the problem https://www.geeksforgeeks.org/problems/overlapping-intervals--170633/1
+# Given an array. The task is to merge all of the overlapping Intervals.
+# Date: 11/11/2024
+# The Problem Consist of two Brute force solution and optimal solution 
+# Method 1 the  Brute Force Method 
+def mergeoverlap(arr):
+    n=len(arr)
+    arr.sort()
+    ans=[]
+    for i in range(n):
+        start,end=arr[i][0],arr[i][1]
+        if ans and end<=ans[-1][1]:
+            continue
+        for j in range(i+1,n):
+            if arr[j][0]<=end:
+                end = max(end,arr[j][1])
+            else:
+                break
+        ans.append([start,end])
+    print(ans)
+arr=[[1, 3], [8, 10], [2, 6], [15, 18]]
+mergeoverlap(arr)
