9baf22ab81f78d4fc2854aa5b035d44f2e3629fe
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-11-22 23:56:01 +0530

    Update medium-leetcode-4Sum.py

--- Arrays-List & String/medium-leetcode-4Sum.py
+++ Arrays-List & String/medium-leetcode-4Sum.py
@@ -47,3 +47,36 @@ def fourSum(nums, target):
 nums = [4, 3, 3, 4, 4, 2, 1, 2, 1, 1]
 target = 9
 fourSum(nums, target)
+
+# ==================== Better Solution ====================
+def four_Sum(nums, target):
+    n = len(nums)
+    ans = []
+    nums.sort()
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        for j in range(i + 1, n):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            k = j + 1
+            l = n - 1
+            while k < l:
+                _sum = nums[i] + nums[j] + nums[k] + nums[l]
+                if _sum == target:
+                    temp = [nums[i], nums[j], nums[k], nums[l]]
+                    ans.append(temp)
+                    k += 1
+                    l -= 1
+                    while k < l and nums[k] == nums[k - 1]:
+                        k += 1
+                    while k < l and nums[l] == nums[l + 1]:
+                        l -= 1
+                elif _sum < target:
+                    k += 1
+                else:
+                    l -= 1
+    print(ans)
+nums = [4, 3, 3, 4, 4, 2, 1, 2, 1, 1]
+target = 9
+ans = four_Sum(nums, target)
