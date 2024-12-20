5f501d9eff9cf3f8d368cee5ba1edc5cd2ba92c5
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2024-12-20 22:08:39 +0530

    Create easy-leetcode-Ransom Note.py

---
+++ Arrays-List & String/easy-leetcode-Ransom Note.py
@@ -0,0 +1,20 @@
+# Ransom Note
+# Problem Link: https://leetcode.com/problems/ransom-note/description/
+# Description: Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+# Date: 20/12/2024
+# The Problem consist of two solution one using brute force: 
+# Brute force solution
+
+def canConstruct(ransomNote, magazine):
+  maga_hash={}
+  for i in magazine:
+    maga_hash[i]=1 + maga_hash.get(i,0)
+    for j in ransomNote:
+      if j not in maga_hash or maga_hash[j] <=0:
+        print("False Not a ransomNote string: ")
+        maga_hash[j] -=1
+      else:
+        print("The given string is a ransomNote ")
+ransomNote= "aa"
+magazine= "ab"
+canConstruct(ransomNote, magazine):
