92870bfb3992929a1f54c83a2d7bd0d5002608c4
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-03 22:12:14 +0530

    Update medium-gfg-Count Subarrays with given XOR.py

--- Arrays-List & String/medium-gfg-Count Subarrays with given XOR.py
+++ Arrays-List & String/medium-gfg-Count Subarrays with given XOR.py
@@ -34,3 +34,21 @@ def subarrayWithXorK(array, target):
 array = [5, 6, 7, 8, 9]
 target = 5
 subarrayWithXorK(array, target)
+
+
+# optimal Solution
+def subArrayWithXorK(arr, tar):
+  l = len(arr)
+  xr = 0
+  mpp = defaultdict(int)
+  mpp[xr] =1
+  count =0
+  for i in range(l):
+    xr = xr ^ arr[i]
+    x = xr ^ tar
+    count += mpp[x]
+    mpp[xr] += 1
+  print("The total number of xor element with subarray K is", cnt)
+arr = [1, 1, 1, 1] 
+tar = 0  
+subArrayWithXorK(arr, tar)
