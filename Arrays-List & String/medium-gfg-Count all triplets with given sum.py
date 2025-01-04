4f65def049d027c05929d062a87bb04d889b05e8
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-04 19:17:53 +0530

    Create medium-gfg-Count all triplets with given sum.py

---
+++ Arrays-List & String/medium-gfg-Count all triplets with given sum.py
@@ -0,0 +1,27 @@
+# Count all triplets with given sum
+# problem link: https://www.geeksforgeeks.org/problems/count-all-triplets-with-given-sum-in-sorted-array/1
+# Given a sorted array arr[] and a target value, the task is to count triplets (i, j, k) of valid indices, such that arr[i] + arr[j] + arr[k] = target and i < j < k
+# Date: 04/01/2025
+# Method 1 Brute force method: 
+def countTriplets(arr, target):
+  c=0
+  # n = len(arr)
+  for i in range(len(arr)-2):
+    j =  i+1
+    k = len(arr)-1
+    while j<k:
+      if arr[j] + arr[k] == target- arr[i]:
+        c +=1
+        temp = j+1
+        while temp<k and arr[temp] == arr[temp-1] :
+          c += 1
+          temp += 1
+          k -= 1
+      elif arr[j] + arr[k] < target-arr[i]:
+        j+=1
+      else:
+          k -= 1
+   print("There are total ", c ,"values of i j and k that equal to sum")
+arr=  [-3, -1, -1, 0, 1, 2]
+target=  -2
+countTriplets(arr, target)
