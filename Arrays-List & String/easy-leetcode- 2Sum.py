1f770a94bbd6a7ef1f3000b028a55a9774480396
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-06 11:52:18 +0530

    Create easy-leetcode- 2Sum.py

---
+++ Arrays-List & String/easy-leetcode- 2Sum.py
@@ -0,0 +1,17 @@
+# Two Sum
+# problem link: https://leetcode.com/problems/two-sum/description/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# Date 06/01/2025
+# This problem can be solved via two solution one is brute force and naive solution while the other is the optimal solution 
+# Brute force solution 
+def twosums(arr,target):
+  for i in range(len(arr)):
+    for j in range(len(arr)):
+      if i == j :
+        continue
+      elif arr[i] + arr[j] == target:
+        print("The two sum that adds upto the index value are",i , j)
+
+arr=[2,7,11,15]
+target = 9
+twosums(arr,target)
