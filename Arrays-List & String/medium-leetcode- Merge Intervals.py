02e941f613bae7ee8a8e754ab4a7416010ef908b
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-10 12:50:06 +0530

    Update medium-leetcode- Merge Intervals.py

--- Arrays-List & String/medium-leetcode- Merge Intervals.py
+++ Arrays-List & String/medium-leetcode- Merge Intervals.py
@@ -3,6 +3,25 @@
 # Given an space seprated string. cound the words in a comma seprated way
 # Date: 18/10/2024
 # The Problem Consist on one solution as of now . 
+# The brute force solution for the problem is: 
+def mergeintervals(arr):
+  arr.sort()
+  ans=[]
+  for i in range(len(arr)):
+    start,end=arr[i][0], arr[i][1]
+    if ans and end <= ans[-1][1]:
+      continue
+    for j in range(i+1 , n):
+      if arr[j][0] <=end:
+        end= max(end, arr[j][1])
+      else:
+        break
+     ans.append([start,end])
+  print(ans)
+arr=[[1,4],[4,5]]
+mergeintervals(arr)
+        
+# Optimal solution
 def merge(intervals):
         intervals.sort(key=lambda x: x[0])
         merged = [intervals[0]]
