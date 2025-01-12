0c79a06a829768dd5b2771586a629d81939b9280
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-12 13:11:33 +0530

    Create medium-leetcode-Find the Duplicate Number.py

---
+++ Arrays-List & String/medium-leetcode-Find the Duplicate Number.py
@@ -0,0 +1,5 @@
+# Find the Duplicate Number
+# Problem Link: https://leetcode.com/problems/find-the-duplicate-number/description/
+# Description: Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive. There is only one repeated number in nums, return this repeated number.
+# Date: 12/1/2025
+
