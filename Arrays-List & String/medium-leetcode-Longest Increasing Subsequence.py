45de1879dd00165b3de6d186a297ecc4553cc6d6
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-13 20:23:02 +0530

    Update medium-leetcode-Longest Increasing Subsequence.py

--- Arrays-List & String/medium-leetcode-Longest Increasing Subsequence.py
+++ Arrays-List & String/medium-leetcode-Longest Increasing Subsequence.py
@@ -14,3 +14,29 @@ def longestoflis(nums):
   print("The longest increasing subsequence is: ",max(dp))
 nums = [10,9,2,5,3,7,101,18]
 longestoflis(nums)
+
+# Optimal Solution
+def longestOfList(arr):
+  res=[]
+  def binarysearch(res, n):
+    left = 0
+    right = len(res)-1
+    while left <= right:
+      mid= (left+right)//2
+      if res[mid] == n:
+        return mid
+      elif res[mid] > n:
+        right = mid- 1
+      else:
+        left = mid + 1
+      return left
+
+  for n in nums:
+    if not res or res[-1] < n:
+      res.append(n)
+    else:
+      ids = binarysearch(res, n)
+      res[ids] = n
+  print(len(res))
+arr= [0,1,0,3,2,3]
+longestOfList(arr)
