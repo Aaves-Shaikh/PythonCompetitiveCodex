536198c496c709c7235859a002dc05c0f4095907
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-23 12:04:16 +0530

    Update medium-leetcode-Kth Largest Element.py

--- Arrays-List & String/medium-leetcode-Kth Largest Element.py
+++ Arrays-List & String/medium-leetcode-Kth Largest Element.py
@@ -4,6 +4,7 @@
 # Date: 23/01/2025
 # The problem will  have three solution 1 Brute Force(Max Heap) 2 Better Solution(Min Heap) 3 Using quick sort
 # Brute Force
+import heapq
 def find_Kth_Largest(arr, k):
   for i in range(len(arr)):
     arr[i] = -arr[i]
@@ -14,3 +15,16 @@ def find_Kth_Largest(arr, k):
 arr = [3,2,1,5,6,4]
 k = 2 #output = 5
 find_Kth_Largest(arr, k)
+
+# Better Solution
+def findkthlargest(array, L):
+  min_heap = []
+  for num in array:
+    if len(min_heap) < L:
+      heapq.heappush(min_heap, num)
+    else:
+      heapq.heappushpop(min_heap, num)
+  print("kth largest element in the array is: ",min_heap[0])
+array= [3,2,3,1,2,4,5,5,6]
+L = 4
+findkthlargest(array, L)
