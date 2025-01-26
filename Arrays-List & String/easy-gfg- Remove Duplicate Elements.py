1924c6ba5bad76189ba55dda9860f86b13108397
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-01-26 21:46:42 +0530

    Update easy-gfg- Remove Duplicate Elements.py

--- Arrays-List & String/easy-gfg- Remove Duplicate Elements.py
+++ Arrays-List & String/easy-gfg- Remove Duplicate Elements.py
@@ -13,6 +13,18 @@ arr1=[1,1, 8, 7, 9,56, 90,99,99,99]
 is_duplicate(arr1)
 
 # Method 2: In this method we simply use set function to check if there are any duplicate values. 
+#  ==================== Better Solution ====================
+# A solution withouy using an hashmap
+def removeDuplicates(self, arr):
+    arr.sort()
+    unique_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] != arr[unique_index]:
+            unique_index += 1
+            arr[unique_index] = arr[i]
+    print("The arrary containing only unique element is: ", unique_index + 1)
+arr = [2, 2, 2, 2, 2]
+removeDuplicates(arr)
 # ==================== Optimal Solution ====================
 def isduplicate(arr):
     if len(arr)!=len(set(arr)):
