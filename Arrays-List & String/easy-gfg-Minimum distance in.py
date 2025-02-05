d77026dba2e74d286a80d7d6d7a15c47d09f5a62
Aaves shiakh <134436811+Aaves-Shaikh@users.noreply.github.com>
2025-02-05 11:21:53 +0530

    Update easy-gfg-Minimum distance in.py

--- Arrays-List & String/easy-gfg-Minimum distance in.py
+++ Arrays-List & String/easy-gfg-Minimum distance in.py
@@ -18,6 +18,34 @@ def mindist(arr, x, y):
   else:
     print(-1)
 arr = [1, 2, 3, 2]
-x = [1]
-y = [2]
+x = 1
+y = 2
 mindist(arr,x,y)
+
+# Brute Force
+
+def minDist(array, num1, num2):
+  n = len(arr)
+  p = -1
+  q = -1
+  min_dist = float('inf')
+  for i in range(n):
+    if arr[i]==num1:
+      p = x
+      if q!= -1:
+        min_dist = min(min_dist, abs(p-q))
+    elif p!= -1:
+      q = num2
+      if p != -1:
+        min_dist = min(min_dist, abs(p-q))
+  if  p == -1 or q == -1:
+    return - 1
+  if min_dist != float('inf')
+    print("the minimum distance between two array is", min_dist)
+  else:
+    print(-1)
+array = [86, 39, 90, 67, 84, 66, 62]
+num1 = 42
+num2 = 12
+minDist(array, num1, num2)
+
